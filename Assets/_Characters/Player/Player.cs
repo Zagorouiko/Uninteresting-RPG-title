c99ca09ea84dde5e460fb0baa3450aab350a2329
Zagorouiko <Zagorouiko@gmail.com>
2020-03-08 12:11:27 -0700

    refactored player/special abilities

--- Assets/_Characters/Player/Player.cs
+++ Assets/_Characters/Player/Player.cs
@@ -3,20 +3,110 @@ using Dragon.Core;
 using Dragon.Weapons;
 using System;
 using System.Collections;
+using Dragon.CameraUI;
 
 
 namespace Dragon.Character
 {
     public class Player : MonoBehaviour
-    {                
+    {
+        float baseDamage = 25f;
+        float lastHitTime = 0f;
+
+        [SerializeField] Weapon weaponInUse;
         [SerializeField] Weapon currentWeaponConfig;     
         [SerializeField] GameObject weaponSocket;
-        
-        GameObject weaponObject;          
+        [SerializeField] AnimatorOverrideController animatorOverrideController;
+
+        [Range(.1f, 1f)]
+        [SerializeField] float criticalHitChance = .1f;
+        [SerializeField] float criticalHitMultiplier = 1.25f;
+
+        SpecialAbilities abilities;
+        GameObject weaponObject;
+        CameraRaycaster cameraRaycaster;
+        Enemy enemy = null;
+        Animator animator;
 
         private void Start()
         {
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+
+            SetAttackAnimation();
+            abilities = GetComponent<SpecialAbilities>();
             PutWeaponInHand(currentWeaponConfig);
+        }
+
+        private void Update()
+        {
+            var healthPercentage = GetComponent<HealthSystem>().healthAsPercentage;
+            if (healthPercentage > Mathf.Epsilon)
+            {
+                ScanForAbilityDown();
+            }
+        }
+
+        private void ScanForAbilityDown()
+        {
+            for (int keyIndex = 1; keyIndex < abilities.GetNumberOfAbilities(); keyIndex++)
+            {
+                if (Input.GetKeyDown(keyIndex.ToString()))
+                {
+                    abilities.AttemptSpecialAbility(keyIndex);
+                }
+            }
+        }
+
+        private void OnMouseOverEnemy(Enemy enemyToSet)
+        {
+            enemy = enemyToSet;
+            if (Input.GetMouseButton(0) && IsTargetInRange(enemy))
+            {
+                AttackTarget(enemy);
+            }
+
+            if (Input.GetMouseButtonDown(1) && IsTargetInRange(enemy))
+            {
+                abilities.AttemptSpecialAbility(0);
+            }
+        }
+
+        private bool IsTargetInRange(Enemy enemy)
+        {
+            float distanceToEnemy = (enemy.transform.position - transform.position).magnitude;
+            return distanceToEnemy <= weaponInUse.GetAttackRange();
+        }
+
+        private void AttackTarget(Enemy enemy)
+        {
+            SetAttackAnimation();
+            if (Time.time - lastHitTime > weaponInUse.GetminTimeBetweenHits())
+            {
+                animator.SetTrigger("New Trigger");
+                enemy.TakeDamage(CalculateDamage());
+                lastHitTime = Time.time;
+            }
+        }
+
+        private float CalculateDamage()
+        {
+            var randomNumber = UnityEngine.Random.Range(0, 1);
+            if (randomNumber <= criticalHitChance)
+            {
+                return (baseDamage + weaponInUse.GetAdditionalDamage()) * criticalHitMultiplier;
+            }
+            else
+            {
+                return (baseDamage + weaponInUse.GetAdditionalDamage());
+            }
+        }
+
+        private void SetAttackAnimation()
+        {
+            animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = animatorOverrideController;
+            animatorOverrideController["Default Attack"] = weaponInUse.GetAttackAnimation();
         }
 
         public void PutWeaponInHand(Weapon weaponToUse)
@@ -28,7 +118,6 @@ namespace Dragon.Character
             weaponObject.transform.localPosition = currentWeaponConfig.gripTransform.localPosition;
             weaponObject.transform.localRotation = currentWeaponConfig.gripTransform.localRotation;
         }
-
     }
 }
 
---
+++ Assets/_Characters/Player/SpecialAbilities.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using Dragon.CameraUI;
+using Dragon.Weapons;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Dragon.Character
+{
+    public class SpecialAbilities : MonoBehaviour
+    {               
+        [SerializeField] AbilityConfig[] abilities;
+        [SerializeField] Image energyOrb;
+        [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float regenPointsPerSecond = 5f;
+
+        public float currentEnergyPoints;
+        float energyAsPercentage { get { return currentEnergyPoints / maxEnergyPoints; } }                    
+
+        void Start()
+        {
+            SetCurrentMaxEnergy();
+            AttachInitialAbilities();
+            UpdateEnergyBar();        
+        }
+
+        void Update()
+        {
+            if (currentEnergyPoints < maxEnergyPoints)
+            {
+                AddEnergyPoints();
+                UpdateEnergyBar();
+            }                   
+        }
+
+        void AttachInitialAbilities()
+        {
+            for (int abilityIndex = 0; abilityIndex < abilities.Length; abilityIndex++)
+            {
+                abilities[abilityIndex].AttachAbilityTo(gameObject);
+            }
+        }
+
+        public int GetNumberOfAbilities()
+        {
+            return abilities.Length;
+        }
+
+        public void AttemptSpecialAbility(int abilityIndex)
+        {
+            var energyCost = abilities[abilityIndex].GetEnergyCost();
+            if (energyCost <= currentEnergyPoints)
+            {
+                UseEnergyPoints(energyCost);
+                //var abilityParams = new AbilityUseParams(enemy, baseDamage);
+                //abilities[abilityIndex].Use(abilityParams);
+            } else
+            {
+                // out of energy sound
+            }
+        }
+
+        private void AddEnergyPoints()
+        {
+            var pointsToAdd = regenPointsPerSecond * Time.deltaTime;
+            currentEnergyPoints = Mathf.Clamp(currentEnergyPoints + pointsToAdd, 0, maxEnergyPoints);
+        }
+
+        private void SetCurrentMaxEnergy()
+        {
+            currentEnergyPoints = maxEnergyPoints;
+        }
+
+        private void UpdateEnergyBar()
+        {
+            energyOrb.fillAmount = energyAsPercentage;
+        }
+
+        public void UseEnergyPoints(float pointsPerHit)
+        {
+            currentEnergyPoints = Mathf.Clamp(currentEnergyPoints - pointsPerHit, 0f, maxEnergyPoints);
+            UpdateEnergyBar();
+        }
+    }
+}
+
