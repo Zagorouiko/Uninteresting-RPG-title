bd52e245c5c9b273ac692ca4d55c748192c2f387
Zagorouiko <Zagorouiko@gmail.com>
2020-03-09 20:53:15 -0700

    Added more components on runtime

--- Assets/_Characters/Character.cs
+++ Assets/_Characters/Character.cs
@@ -5,31 +5,32 @@ using Dragon.CameraUI;
 
 namespace Dragon.Character
 {
-    [RequireComponent(typeof(NavMeshAgent))]
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(CapsuleCollider))]
-    [RequireComponent(typeof(Animator))]
     public class Character : MonoBehaviour
     {
-        [Header("Capsule Collider Settings")]
+        [Header("Audio")]
+        [SerializeField] float audioSourceSpatialBlend = 0.5f;
+
+        [Header("Capsule Collider")]
         [SerializeField] Vector3 capsuleColliderCenter = new Vector3(0, 1.03f, 0);
         [SerializeField] float capsuleColliderRadius = 02f;
         [SerializeField] float capsuleColliderHeight = 2.03f;
 
-        [Header("Setup Settings")]
+        [Header("Animator")]
         [SerializeField] RuntimeAnimatorController animatorController;
         [SerializeField] AnimatorOverrideController animatorOverrideController;
         [SerializeField] Avatar characterAvatar;
         
 
-        [Header("Movement Properties")]
-        [SerializeField] float stoppingDistance = 1f;
+        [Header("Movement")]
         [SerializeField] float moveSpeedMultiplier = 2f;
         [SerializeField] float animationSpeedMultiplier = 2f;
         [SerializeField] float movingTurnSpeed = 360;
         [SerializeField] float stationaryTurnSpeed = 180;
         [SerializeField] float moveThreshold = 1f;
 
+        [Header("Nav Mesh")]
+        [SerializeField] float navMeshAgentSteeringSpeed = 1.0f;
+        [SerializeField] float NavMeshAgentStoppingDistance = 1.3f;
 
         float turnAmount;
         float forwardAmount;
@@ -38,7 +39,7 @@ namespace Dragon.Character
         CapsuleCollider capsuleCollider;
         Animator animator;
         Rigidbody rigidBody;
-        NavMeshAgent agent;
+        NavMeshAgent navMeshAgent;
 
 
         void Awake()
@@ -48,28 +49,33 @@ namespace Dragon.Character
 
         private void AddRequiredComponents()
         {
+            var audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.spatialBlend = audioSourceSpatialBlend;
+
+            rigidBody = gameObject.AddComponent<Rigidbody>();
+            rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+
             animator = gameObject.AddComponent<Animator>();
             capsuleCollider = gameObject.AddComponent<CapsuleCollider>();
             animator.avatar = characterAvatar;
+            animator.runtimeAnimatorController = animatorController;
             
             capsuleCollider.center = capsuleColliderCenter;
             capsuleCollider.radius = capsuleColliderRadius;
             capsuleCollider.height = capsuleColliderHeight;
+
+            navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+            navMeshAgent.speed = navMeshAgentSteeringSpeed;
+            navMeshAgent.autoBraking = false;
+            navMeshAgent.updateRotation = false;
+            navMeshAgent.updatePosition = true;
+            navMeshAgent.stoppingDistance = NavMeshAgentStoppingDistance;
         }
 
         void Start()
         {
             CameraRaycaster cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-            
-            rigidBody = GetComponent<Rigidbody>();
-
-            rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
             animator.applyRootMotion = true;
-
-            agent = GetComponent<NavMeshAgent>();
-            agent.updateRotation = false;
-            agent.updatePosition = true;
-            agent.stoppingDistance = stoppingDistance;
 
             cameraRaycaster.onMouseOverWalkable += OnMouseOverPotentiallyWalkable;
             cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
@@ -77,9 +83,9 @@ namespace Dragon.Character
 
         void Update()
         {
-            if (agent.remainingDistance > agent.stoppingDistance)
+            if (navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
             {
-                Move(agent.desiredVelocity);
+                Move(navMeshAgent.desiredVelocity);
             }
             else
             {
@@ -128,7 +134,7 @@ namespace Dragon.Character
         {
             if (Input.GetMouseButton(0))
             {
-                agent.SetDestination(destination);
+                navMeshAgent.SetDestination(destination);
             }
         }
 
@@ -136,7 +142,7 @@ namespace Dragon.Character
         {
             if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
             {
-                agent.SetDestination(enemy.transform.position);
+                navMeshAgent.SetDestination(enemy.transform.position);
             }
         }
 
